df7526e31ac5e5d5de00aaac44272d839101c7c7
Surneval <nadia.trusova@yahoo.com>
2022-10-12 23:48:17 +0300

    Example005 Special User

---
+++ Exapmle005_SpecialUser/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Please type your name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "jack")
+{
+    Console.WriteLine("Wow! It's you, Jack!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.Write(username);
+}

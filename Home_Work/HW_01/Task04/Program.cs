4f3281390669120d5f6642ce5f2caad59d6dc477
Surneval <nadia.trusova@yahoo.com>
2022-10-14 18:29:44 +0300

    even array

---
+++ Home_Work/HW_01/Task04/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Please type any number N >> ");
+string input = Console.ReadLine();
+int n = Convert.ToInt32(input);
+int i = 1;
+if (n < 0)
+{
+    while (n < i)
+    {
+        if (n % 2 == 0)
+        {
+            Console.Write($"{n}, ");
+            
+        }
+        n++;
+    }
+}
+if (n > 0)
+{
+    while (i < n + 1)
+    {
+        if (i % 2 == 0)
+        {
+            Console.Write($"{i}, ");
+        }
+        i++;
+    }
+}

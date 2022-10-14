c5a3ec301514f3a92ef1a2dbda78206f5c7f9b03
Surneval <nadia.trusova@yahoo.com>
2022-10-14 16:17:25 +0300

    X^3

---
+++ Lesson01_04/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу вычисления модуля числа.
+System.Console.Write("Please type the number >");
+int number = int.Parse(System.Console.ReadLine());
+int mod = number;
+if (number<0)
+{
+    mod = -number;
+}
+System.Console.WriteLine($"{mod} is abs of {number}");

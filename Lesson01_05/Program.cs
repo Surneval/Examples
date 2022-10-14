4dd2d0296caeababb29e7482c0c8ffea26baf1e9
Surneval <nadia.trusova@yahoo.com>
2022-10-14 16:19:21 +0300

    Reciprocal

---
+++ Lesson01_05/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая на вход принимает значение, а  на выходе показывает обратное значение.
+Console.Write("Please type the number > ");
+string inputString = Console.ReadLine();
+double number = Convert.ToDouble(inputString);
+double reciprocal = 1 / number;
+Console.WriteLine($"{reciprocal} is reciprocal number of {number}");

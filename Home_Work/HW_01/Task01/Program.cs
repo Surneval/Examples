b950ad1065a76bae8f15badb42ff675399c8f6c1
Surneval <nadia.trusova@yahoo.com>
2022-10-14 16:36:41 +0300

    Maximum and minimum

---
+++ Home_Work/HW_01/Task01/Program.cs
@@ -0,0 +1,13 @@
+﻿// SНапишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.WriteLine("Please type any number to be considered number1>> ");
+Double number1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Please type any number to be considered number2 >> ");
+Double number2 = Convert.ToDouble(Console.ReadLine());
+if (number1 > number2)
+{
+    Console.WriteLine($"{number1} is maximim, {number2} is minimum");
+}
+if (number2 > number1)
+{
+    Console.WriteLine($"{number2} is maximum, {number1} is minimum");
+}

8ac6dd504c79e8fa005a135277c47cd852b23c61
Surneval <nadia.trusova@yahoo.com>
2022-10-14 16:16:27 +0300

    Y=x^3

---
+++ Lesson01_03_01/Program.cs
@@ -0,0 +1,5 @@
+﻿// 3. Напишите программу вычисления функции: y = x^3
+Console.WriteLine("Please type number x >> ");
+double x = Convert.ToDouble(Console.ReadLine());
+double y = x * x * x;
+Console.WriteLine ($"{y} is cube function of x ");

f32968317f9d522511286c56b3d7852594b5ce6b
Surneval <nadia.trusova@yahoo.com>
2022-10-17 13:13:23 +0300

    index of array

---
+++ Lection2/Task04_index/Program.cs
@@ -0,0 +1,17 @@
+﻿// Working with index of array
+int[] array = { 11, 22, 36, 42, 5, 67, 7, 18, 36, 18 };
+int n = array.Length;
+int find = 18;
+
+int index = 0;
+while(index<n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index+1
+    index++;
+
+}
